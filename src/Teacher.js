4fe426a1200354e60c8fcbd0b0cd113c23f30514
Raul Savianu <raul.savianu@tba.group>
2020-06-20 15:33:22 +0300

    Added test list to teacher page

--- src/Teacher.js
+++ src/Teacher.js
@@ -3,11 +3,26 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import TestItem from './TestItem';
 
 class Teacher extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            tests: []
+        }
     }
+
+    componentDidMount() {
+        fetch("http://localhost:8090/test", {
+            method: 'GET'
+        })
+            .then(res => res.json())
+            .then(
+                res => this.setState({ tests: res })
+            )
+    }
+
     render() {
         return (
             <div>
@@ -19,14 +34,29 @@ class Teacher extends React.Component {
                             </Typography>
                             <Button color="inherit" onClick={(event) => this.logout(event)}>Logout</Button>
                         </AppBar>
+                        <br />
+                        {this.tests()}
                     </div>
                 </MuiThemeProvider>
             </div>
         );
     }
+
     logout(event) {
         sessionStorage.clear();
         window.location.reload(false);
+    }
+
+    tests() {
+        return (
+            <div>
+                <Typography style={{marginLeft: 15}} variant="h4" color="inherit">
+                    Tesztek
+                </Typography>
+                <br/>
+                {this.state.tests.map(test => <TestItem id={test.id} name={test.name} />)}
+            </div>
+        );
     }
 
 }
--- src/TestItem.js
+++ src/TestItem.js
@@ -1,12 +1,47 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import RaisedButton from 'material-ui/RaisedButton';
 
-function TestItem(props){
-    return(
-        <tr>
-            <td>{props.item.id}</td>
-            <td>{props.item.name}</td>
-        </tr>
+function TestItem(props) {
+    const classes = useStyles();
+    return (
+        <ExpansionPanel>
+            <ExpansionPanelSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+            >
+                <Typography className={classes.heading}>{props.id + " " + props.name}</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+                <RaisedButton label="Start" primary={true} onClick={(event) => this.start(event)} />
+                <RaisedButton label="Edit" style={{marginLeft: 10}} primary={true} onClick={(event) => this.edit(event)} />
+            </ExpansionPanelDetails>
+        </ExpansionPanel>
     )
 }
+
+function start() {
+
+}
+
+function edit() {
+
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
 
 export default TestItem;
